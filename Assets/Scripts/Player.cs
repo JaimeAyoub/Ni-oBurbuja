f52701791fe919813881bf9b2080500fd8f8fde9
Jaime <ayoubramosjaime@gmail.com>
2025-01-25 14:38:53 -0600

    ScriptPlayerDerrape

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
 
 public class Player : MonoBehaviour
 {
 
     public Rigidbody2D rb;
-    public float speedX; 
-    public PhysicMaterial physicMaterial;
+
+    private float inputX;
+
+
+    public float speedX;
+    public float speedlimit;
+    public float speedlimitNegative;
+
+
+    public Vector2 friction;
+    private float TotalVelocity;
 
     void Start()
     {
-        if(rb == null)
+        if (rb == null)
             rb = GetComponent<Rigidbody2D>();
-
+        speedlimitNegative = -speedlimit;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        //inputXRight = Input.GetAxis("Horizontal");
         Move();
+        Debug.Log(inputX);
     }
 
     private void Move()
     {
-       rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0);     
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (inputX <= speedlimit)
+            {
+                inputX += 0.05f;
+
+            }
+            else if (inputX > speedlimit)
+            {
+                inputX = speedlimit;
+            }
+
+            TotalVelocity = (speedX * inputX);
+        }
+        else
+        {
+            if (inputX > 0)
+            {
+                inputX -= 0.005f;
+                TotalVelocity = (speedX * inputX);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (inputX >= -speedlimit)
+            {
+                inputX -= 0.05f;
+
+            }
+            else if (inputX < -speedlimit)
+            {
+                inputX = -speedlimit;
+            }
+
+            TotalVelocity = (speedX * inputX);
+        }
+        else
+        {
+            if (inputX < 0)
+            {
+                inputX += 0.005f;
+                TotalVelocity = (speedX * inputX);
+            }
+        }
+
+        if (TotalVelocity > 0)
+            TotalVelocity -= friction.x;
+        if (TotalVelocity <= 0)
+            TotalVelocity += friction.x;
+
+
+
+        rb.velocity = new Vector2(TotalVelocity, 0);
     }
 }
