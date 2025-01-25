9f51e74e836dbc515fb554200fa96e006367f9bf
JaimeAyoub <ayoubramosjaime@gmail.com>
2025-01-24 22:55:58 -0600

    CollisionTile cambiada y player Scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,23 @@ public class Player : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public float speedX; 
 
     void Start()
     {
-        
+        if(rb == null)
+            rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
     }
 }
