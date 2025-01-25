804b559b60f82e2099d655b37477a70032db6bfe
JaimeAyoub <ayoubramosjaime@gmail.com>
2025-01-24 20:48:22 -0600

    First

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
