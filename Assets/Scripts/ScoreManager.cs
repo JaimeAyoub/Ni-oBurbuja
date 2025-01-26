4f42227ce78c534ebb2ed125135b8e1e7763df38
AlexG <aleguztej00@gmail.com>
2025-01-26 02:07:00 -0600

    WeaFlashJEJE

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEditor.Experimental.GraphView;
+using DG.Tweening;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -15,21 +16,36 @@ public class ScoreManager : MonoBehaviour
 
     void Start()
     {
-        highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
-    }
+        //highScoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
 
-    private void Update()
-    {
-        SetHighestScore(); 
+        WeaAttack();
     }
 
     public void SetHighestScore()
     {
         actualScore = playerScore.GetScore();
-        if(actualScore > PlayerPrefs.GetInt("HighScore", 0))
+        if (actualScore > PlayerPrefs.GetInt("HighScore", 0))
         {
             PlayerPrefs.SetInt("HighScore", actualScore);
             highScoreText.text = actualScore.ToString();
         }
     }
+
+    void WeaAttack()
+    {
+        StartCoroutine(FadeCuadrantes(Random.Range(0, cuadrantes.Length)));
+    }
+
+    IEnumerator FadeCuadrantes(int random)
+    {
+        if (cuadrantes.Length > random)
+        {
+            SpriteRenderer spriteRenderer = cuadrantes[random].GetComponent<SpriteRenderer>();
+            Color initialColor = spriteRenderer.color;
+            spriteRenderer.color = new Color(initialColor.r, initialColor.g, initialColor.b, 1f);
+            spriteRenderer.DOFade(0f, 1f).SetLoops(5, LoopType.Yoyo);  
+        }
+        yield return new WaitForSeconds(4f);  
+    }
+
 }
